0f23c6582e778370604e106a02012020f6cc9ed6
vale <vlatapie2mail.com>
2023-12-10 22:27:51 -0300

    feat-01-creacion de navbar y sus comoponentes

---
+++ src/assets/componentes/general/link/Link.jsx
@@ -0,0 +1,12 @@
+
+const Link = ({title}) => {
+  return (
+    <li className="nav-item">
+        <a className="nav-link" 
+           href="#">{title}
+        </a>
+    </li>
+  )
+}
+
+export default Link
